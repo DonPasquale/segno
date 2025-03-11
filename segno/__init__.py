2fa570f8c6306493c0ebec1024d8145e58d8643b
Lars <heuer@semagia.com>
2025-03-11 20:54:40 +0100

    Incremented development version

--- segno/__init__.py
+++ segno/__init__.py
@@ -15,7 +15,7 @@ from . import encoder
 from .encoder import DataOverflowError
 from . import writers, utils
 
-__version__ = '1.6.4'
+__version__ = '1.6.5.dev'
 
 __all__ = ('make', 'make_qr', 'make_micro', 'make_sequence', 'QRCode',
            'QRCodeSequence', 'DataOverflowError')
